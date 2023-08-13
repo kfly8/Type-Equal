ad6bbbc643b20c3070d6b5d9938e9a47be2463fb
kobaken <kentafly88@gmail.com>
2023-08-13 20:35:35 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Types::Eq - It's new $module
+
+# SYNOPSIS
+
+    use Types::Eq;
+
+# DESCRIPTION
+
+Types::Eq is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/Types/Eq.pm
@@ -0,0 +1,39 @@
+package Types::Eq;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Types::Eq - It's new $module
+
+=head1 SYNOPSIS
+
+    use Types::Eq;
+
+=head1 DESCRIPTION
+
+Types::Eq is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
